7c50bf5ca7212f9928287bf006d15d224ef4a0a0
donggi chae <69336797+DonggiChae@users.noreply.github.com>
2023-05-25 13:50:12 +0900

    undate auth

--- src/app/personal/layout.tsx
+++ src/app/personal/layout.tsx
@@ -1,14 +1,26 @@
+"use client";
+
 import React from "react";
 import PersonalSideBar from "@/components/sideBars/PersonalSideBar";
 import Profile from "@/components/personal/Profile";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+import { Amplify, withSSRContext } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+import awsExports from "../../aws-exports";
+
+Amplify.configure({ ...awsExports, ssr: true });
+
+function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-row">
-      <div className="fixed z-40 pt-14">
+    <div className="flex flex-row pt-14">
+      <div className="fixed z-40 ">
         <PersonalSideBar />
       </div>
-      <div className="w-screen bg-zinc-100 pl-80 pt-14">{children}</div>
+      <div className="w-screen bg-zinc-100 pl-80 ">{children}</div>
     </div>
   );
 }
+
+export default withAuthenticator(layout);
--- src/app/personal/page.tsx
+++ src/app/personal/page.tsx
@@ -1,23 +1,9 @@
 "use client";
 
 import PersonalMain from "@/components/personal/PersonalMain";
-import { Amplify, withSSRContext } from "aws-amplify";
 import { headers } from "next/headers";
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
-
-import awsExports from "../../aws-exports";
-
-Amplify.configure({ ...awsExports, ssr: true });
 
 function Page() {
-  // const req = {
-  //   headers: {
-  //     cookie: headers().get("cookie"),
-  //   },
-  // };
-
-  // const SSR = withSSRContext({ req });
   return (
     <div className="flex h-full w-full  items-center justify-center">
       <PersonalMain />
@@ -25,4 +11,4 @@ function Page() {
   );
 }
 
-export default withAuthenticator(Page);
+export default Page;
